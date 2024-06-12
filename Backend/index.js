4aba9a508a776fee76782d85e3cf7effd0901e39
Vince-arch <vincekimani006@gmail.com>
2024-06-12 20:13:19 +0300

    adding route to update a book

--- Backend/index.js
+++ Backend/index.js
@@ -56,6 +56,32 @@ app.get('/books', async(req, res)=>{
     }
 })
 
+//Route to update a book
+app.put('/books:id', async(req, res)=>{
+    try {
+        if(
+            !req.body.title ||
+            !req.body.author ||
+            !req.body.publishYear   
+          ) {
+              return res.status(400).send({
+                  message: 'send all required fields: title,author, publishYear'});
+          }
+
+          const {id} = request.params;
+
+          const result = await Book.findByIdAndUpdate(id, req.body);
+
+          if (!result){
+            return res.status(404).json({message:'Book not found'});
+          }
+          return res.status(200).send({message: 'Book updated successfully'})
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).send({message:error.message})
+    }
+})
+
 mongoose
 .connect(mongoDBURL)
 .then(()=>{
