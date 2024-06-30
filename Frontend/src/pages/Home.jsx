94e0542938707b1e8eb08991d8f4cece2454c9c9
Vince-arch <vincekimani006@gmail.com>
2024-06-30 20:27:45 +0300

    adding changes

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
+import Spinner from '../components/Spinner';
+import {Link} from 'react-router-dom';
+import {AiOutlineEdit} from 'react-icons/ai';
+import {BsInfoCircle} from 'react-icons/bs'
+import {MdOutlineAddbox, MdOutlineDelete} from 'react-icons/md';
 
 const Home = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(()=>{
+    setLoading(true);
+    axios
+    .get('http://localhost:55555/books')
+    .then((res)=>{setBooks(res.data.data);
+    setLoading(false);
+    })
+  },[])
   return (
     <div>Home</div>
   )
